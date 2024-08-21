d033f37ed2ee4c4ff951e83fe12673575fe07393
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-08-21 17:00:29 +0200

    Delete instructor by id

--- src/main/java/eu/kudljo/cruddemo/CruddemoApplication.java
+++ src/main/java/eu/kudljo/cruddemo/CruddemoApplication.java
@@ -19,8 +19,18 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 		return runner -> {
 //			createInstructor(appDAO);
-			findInstructor(appDAO);
+//			findInstructor(appDAO);
+			deleteInstructor(appDAO);
 		};
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+		int id = 1;
+		System.out.println("Deleting instructor id: " + id);
+
+		appDAO.deleteInstructorById(id);
+
+		System.out.println("Done!");
 	}
 
 	private void findInstructor(AppDAO appDAO) {
--- src/main/java/eu/kudljo/cruddemo/dao/AppDAO.java
+++ src/main/java/eu/kudljo/cruddemo/dao/AppDAO.java
@@ -7,4 +7,6 @@ public interface AppDAO {
     void save(Instructor instructor);
 
     Instructor findInstructorById(int id);
+
+    void deleteInstructorById(int id);
 }
