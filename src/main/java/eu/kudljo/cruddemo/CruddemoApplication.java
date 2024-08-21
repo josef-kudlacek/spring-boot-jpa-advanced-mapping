f7b3fb75513be759953336837e94049c72bdc64d
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-08-21 16:02:00 +0200

    Initial commit

---
+++ src/main/java/eu/kudljo/cruddemo/CruddemoApplication.java
@@ -0,0 +1,16 @@
+package eu.kudljo.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+}
