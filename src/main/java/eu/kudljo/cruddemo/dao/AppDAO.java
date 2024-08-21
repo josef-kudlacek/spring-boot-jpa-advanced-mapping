4dafa49cd512dcf6919cb1c5c27265b286a0c077
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-08-21 16:30:31 +0200

    Create DAO layer

---
+++ src/main/java/eu/kudljo/cruddemo/dao/AppDAO.java
@@ -0,0 +1,8 @@
+package eu.kudljo.cruddemo.dao;
+
+import eu.kudljo.cruddemo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor instructor);
+}
---
+++ src/main/java/eu/kudljo/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,20 @@
+package eu.kudljo.cruddemo.dao;
+
+import eu.kudljo.cruddemo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.transaction.annotation.Transactional;
+
+public class AppDAOImpl implements  AppDAO {
+
+    private EntityManager entityManager;
+
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        entityManager.persist(instructor);
+    }
+}
