0e449ab015af6c3379eb040516ab6b223e20349d
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-08-30 11:10:18 +0200

    Delete course

--- src/main/java/eu/kudljo/cruddemo/CruddemoApplication.java
+++ src/main/java/eu/kudljo/cruddemo/CruddemoApplication.java
@@ -20,7 +20,8 @@ public class CruddemoApplication {
 //			createCourseAndStudents(appDAO);
 //			findCourseAndStudents(appDAO);
 //			findStudentAndCourses(appDAO);
-			addMoreCoursesForStudent(appDAO);
+//			addMoreCoursesForStudent(appDAO);
+			deleteCourse(appDAO);
 		};
 	}
 
